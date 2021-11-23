2124aed2c6b8fc73e87e3ccb9fdec85c544dfbcc
Leonardo Chuello <ljchuello@gmail.com>
2021-11-23 12:59:51 -0500

    .

--- MsSqlServerMvc/Libreria/Granular.cs
+++ MsSqlServerMvc/Libreria/Granular.cs
@@ -26,10 +26,10 @@ namespace MsSqlServerMvc.Libreria
                 stringBuilder.AppendLine("using System.Text;");
                 stringBuilder.AppendLine("using System.Threading.Tasks;");
                 stringBuilder.AppendLine("");
-                stringBuilder.AppendLine("namespace Granular");
+                stringBuilder.AppendLine("namespace Biblioteca.SqlServer");
                 stringBuilder.AppendLine("{");
 
-                stringBuilder.AppendLine($"    public class o{tabla}");
+                stringBuilder.AppendLine($"    public class O{tabla}");
                 stringBuilder.AppendLine("    {");
 
                 #region Insert
@@ -86,19 +86,117 @@ namespace MsSqlServerMvc.Libreria
                         if (iterator != list.Count)
                         {
                             // No nulo - No ultimo
-                            stringBuilder.AppendLine($"         stringBuilder.AppendLine($\"'{{Tools.Remplazar(Mo{tabla}.{row.Nombre})}})', -- {row.Nombre} | {row.TipoDotNet} | {row.TipoSql}\");");
+                            stringBuilder.AppendLine($"         stringBuilder.AppendLine($\"'{{Tools.Remplazar(Mo{tabla}.{row.Nombre})}}', -- {row.Nombre} | {row.TipoDotNet} | {row.TipoSql}\");");
                         }
                         else
                         {
                             // No nulo - Si ultimo
                             stringBuilder.AppendLine($"         stringBuilder.AppendLine($\"'{{Tools.Remplazar(Mo{tabla}.{row.Nombre})}}' -- {row.Nombre} | {row.TipoDotNet} | {row.TipoSql}\");");
                         }
-                        
+
                     }
                 }
                 stringBuilder.AppendLine("          stringBuilder.AppendLine(\");\");");
                 stringBuilder.AppendLine("          return stringBuilder.ToString();");
                 stringBuilder.AppendLine("      }");
+
+                #endregion
+
+                #region Update Block
+
+                stringBuilder.AppendLine("");
+                stringBuilder.AppendLine($"        public static string Update(Mo{tabla} Mo{tabla})");
+                stringBuilder.AppendLine($"        {{");
+                stringBuilder.AppendLine($"            StringBuilder stringBuilder = new StringBuilder();");
+                stringBuilder.AppendLine($"            stringBuilder.AppendLine(\"\");");
+                stringBuilder.AppendLine($"            stringBuilder.AppendLine(\"--  Update {tabla}\");");
+                stringBuilder.AppendLine($"            stringBuilder.AppendLine(\"UPDATE {tabla} SET\");");
+                iterator = 0;
+                foreach (var row in list.Where(x => x.Pk == false))
+                {
+                    iterator++;
+
+                    // Ultimo
+                    if (list.Count(x => x.Pk == false) != iterator)
+                    {
+                        if (row.Nulo == false)
+                        {
+                            // No nulo - No ultimo
+                            stringBuilder.AppendLine($"            stringBuilder.AppendLine($\"{row.Nombre} = '{{Tools.Remplazar(Mo{tabla}.{row.Nombre})}}', -- {row.Nombre} | {row.TipoDotNet} | {row.TipoSql}\");");
+                        }
+                        else
+                        {
+                            // Si nulo - No ultimo
+                            stringBuilder.AppendLine($"         stringBuilder.AppendLine(!Cadena.Vacia(Mo{tabla}.{row.Nombre})");
+                            stringBuilder.AppendLine($"             ? $\"{row.Nombre} = '{{Tools.Remplazar(Mo{tabla}.{row.Nombre})}}', -- {row.Nombre} | {row.TipoDotNet} | {row.TipoSql}\"");
+                            stringBuilder.AppendLine($"             : $\"{row.Nombre} = NULL, -- {row.Nombre} | {row.TipoDotNet} | {row.TipoSql}\");");
+                        }
+
+                    }
+                    else
+                    {
+                        if (row.Nulo == false)
+                        {
+                            // No nulo - No ultimo
+                            stringBuilder.AppendLine($"            stringBuilder.AppendLine($\"{row.Nombre} = '{{Tools.Remplazar(Mo{tabla}.{row.Nombre})}}' -- {row.Nombre} | {row.TipoDotNet} | {row.TipoSql}\");");
+                        }
+                        else
+                        {
+                            // Si nulo - No ultimo
+                            stringBuilder.AppendLine($"         stringBuilder.AppendLine(!Cadena.Vacia(Mo{tabla}.{row.Nombre})");
+                            stringBuilder.AppendLine($"             ? $\"{row.Nombre} = '{{Tools.Remplazar(Mo{tabla}.{row.Nombre}}}' -- {row.Nombre} | {row.TipoDotNet} | {row.TipoSql}\"");
+                            stringBuilder.AppendLine($"             : $\"{row.Nombre} = NULL -- {row.Nombre} | {row.TipoDotNet} | {row.TipoSql}\");");
+                        }
+                    }
+
+                }
+                stringBuilder.AppendLine($"            stringBuilder.AppendLine(\"WHERE\");");
+                iterator = 0;
+                foreach (var row in list.Where(x => x.Pk == true))
+                {
+                    iterator++;
+                    if (list.Count(x => x.Pk) != iterator)
+                    {
+                        stringBuilder.AppendLine($"            stringBuilder.AppendLine($\"{row.Nombre} = '{{Tools.Remplazar(Mo{tabla}.{row.Nombre})}}' AND -- {row.Nombre} | {row.TipoDotNet} | {row.TipoSql}\");");
+                    }
+                    else
+                    {
+                        stringBuilder.AppendLine($"            stringBuilder.AppendLine($\"{row.Nombre} = '{{Tools.Remplazar(Mo{tabla}.{row.Nombre})}}'; -- {row.Nombre} | {row.TipoDotNet} | {row.TipoSql}\");");
+                    }
+
+                }
+                stringBuilder.AppendLine($"        return stringBuilder.ToString();");
+                stringBuilder.AppendLine($"        }}");
+
+                #endregion
+
+                #region Delete Block
+
+                iterator = 0;
+                stringBuilder.AppendLine("");
+                stringBuilder.AppendLine($"        public static string Delete(Mo{tabla} Mo{tabla})");
+                stringBuilder.AppendLine($"        {{");
+                stringBuilder.AppendLine($"            StringBuilder stringBuilder = new StringBuilder();");
+                stringBuilder.AppendLine($"            stringBuilder.AppendLine(\"\");");
+                stringBuilder.AppendLine($"            stringBuilder.AppendLine(\"--  Delete {tabla}\");");
+                stringBuilder.AppendLine($"            stringBuilder.AppendLine(\"DELETE FROM {tabla} WHERE\");");
+                foreach (var row in list.Where(x => x.Pk))
+                {
+                    iterator++;
+                    if (list.Count(x => x.Pk) != iterator)
+                    {
+                        // No ultimo
+                        stringBuilder.AppendLine($"            stringBuilder.AppendLine($\"{row.Nombre} = '{{Tools.Remplazar(Mo{tabla}.{row.Nombre})}}' AND -- {row.Nombre} | {row.TipoDotNet} | {row.TipoSql}\");");
+                    }
+                    else
+                    {
+                        // Ultimo
+                        stringBuilder.AppendLine($"            stringBuilder.AppendLine($\"{row.Nombre} = '{{Tools.Remplazar(Mo{tabla}.{row.Nombre})}}'; -- {row.Nombre} | {row.TipoDotNet} | {row.TipoSql}\");");
+                    }
+                }
+                stringBuilder.AppendLine($"        return stringBuilder.ToString();");
+                stringBuilder.AppendLine($"        }}");
+                
 
                 #endregion
 
